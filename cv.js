1737b2122b6e74baedef06d760202e9070f16852
rada-i <neposebanmail@gmail.com>
2023-04-18 01:17:22 +0200

    initial commit

---
+++ cv.js
@@ -0,0 +1,47 @@
+let mouseX = 0;
+let mouseY = 0;
+let flashlight = document.getElementById("flashlight");
+
+let flashlightOn = false;
+
+const isTouchDevice = () => {
+  try {
+    document.createEvent("TouchEvent");
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+function getMousePosition(e) {
+  if (flashlightOn) {
+    mouseX = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+    mouseY = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+
+    flashlight.style.setProperty("--Xpos", mouseX + "px");
+    flashlight.style.setProperty("--Ypos", mouseY + "px");
+  }
+}
+
+function toggleFlashlight() {
+  flashlightOn = !flashlightOn;
+  if (!flashlightOn) {
+    flashlight.style.display = "none";
+  } else {
+    flashlight.style.display = "block";
+  }
+}
+
+flashlight.style.display = "none";
+
+document.addEventListener("mousemove", getMousePosition);
+document.addEventListener("touchmove", getMousePosition);
+
+const now = new Date();
+const span = document.createElement("span");
+
+span.textContent = dateFns.format(now, "MMMM, YYYY. ");
+
+const existingSpan = document.querySelector("span");
+
+existingSpan.parentNode.replaceChild(span, existingSpan);
